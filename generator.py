bd1f06e00a6352ee8e73d6355100c66976f1b92f
Nate Schwartz <156002099+NaSchwartz@users.noreply.github.com>
2025-06-04 21:10:59 +0000

    singles generation WIP

---
+++ generator.py
@@ -0,0 +1,20 @@
+# Grid size
+size = 4
+
+def print_moves(move_list):
+    for i in move_list:
+        print(i)
+
+def singles(num:str):
+    moves = []
+    index = 0
+    for i in range(num.count("1")):
+        moves.append()
+        index = num.index("1")
+
+
+# horizontal moves
+
+# vertical moves
+
+# diaganol moves
